87a971d5848519d3db3eb8dd38d170c816e05706
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2023-12-30 17:24:45 +0530

    Add files via upload

---
+++ write a program of selection sort in array.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void  selection_sort(int arr[], int n){
+for(int i=0;i<n-1;i++)
+{
+    int minIndex=i;
+    for(int j=i+1;j<n ;j++)
+    {
+        if(arr[j]<arr[ minIndex])
+        {
+            minIndex=j;
+        }
+    }
+    swap(arr[ minIndex],arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+    cout<<arr[i]<<"  ";
+}
+
+
+ 
+}
+
+int main(){
+      int n;
+    cout<<"Enter the size of array";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the array element "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    selection_sort(arr,n);
+   
+
+return 0;
+}
